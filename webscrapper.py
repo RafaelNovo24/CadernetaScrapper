0fbe42cdfd96928bc5c3d73dde9506f07aec3eef
Rafael Novo <rafaelnovo24@gmail.com>
2026-09-03 23:16:42 +0100

    updated code

--- webscrapper.py
+++ webscrapper.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 CADERNETA_SITE = r"https://www.predialonline.pt/PredialOnline/FRM005RPOLCP_input.action"
-BUILDING_CODE = "PA-3267-07514-131728-007612"
+BUILDING_CODE = "PP-3487-09722-131713-007612"
 #   pyinstaller --onefile --windowed --add-data "env/Lib/site-packages/playwright/driver:playwright/driver" webscrapper.py
 
 
